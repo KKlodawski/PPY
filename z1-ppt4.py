08bc8a59417d54ebfe76685f2c6005a0da7c579c
KKlodawski <s24777@pjwstk.edu.pl>
2023-03-13 21:58:17 +0100

    Podpunkt 4

---
+++ z1-ppt4.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import webbrowser
+import requests
+
+pageurl = input("Podaj adres url: ")
+for x in range(3):
+    date = (datetime.today()).replace(year=datetime.today().year-(x+1)).strftime('%Y%m%d')
+    url = "http://archive.org/wayback/available?url=" + pageurl + "&timestamp=" + str(date)
+    response = requests.get(url)
+    d = response.json()
+    page = d["archived_snapshots"]["closest"]["url"]
+    print("Wersja sprzed lat : " + str(x+1) + " : " + page)
+
